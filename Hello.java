373bead50effa8a645145088858ff807504a65e7
Kentca2 <59925894+Kentca2@users.noreply.github.com>
2020-01-15 10:14:34 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*********************************************************
+* Hello.java
+**********************************************************
+* Cheyenne Kent
+* January 15, 2020
+* CMSC 255-003
+*********************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		System.out.println("Cheyenne kent");
+		System.out.println("Hello.java");
+		System.out.println("we are installing java onto our computers and to make sure we can run the program on the computers");
+		System.out.println("1-15-2020");
+		System.out.println("CMSC 255-003");
+		System.out.println();
+	}
+}
