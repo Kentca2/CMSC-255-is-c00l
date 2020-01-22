b4667d3506b5655ec31fce53153b6553bb323259
Kentca2 <59925894+Kentca2@users.noreply.github.com>
2020-01-22 10:23:33 -0500

    Add files via upload

---
+++ Names.java
@@ -0,0 +1,7 @@
+public class Names{
+	public static void main(String[] args){
+		System.out.println("----------------------------------------------");
+		System.out.println("This program was written by Cheyenne and Kalah");
+		System.out.println("----------------------------------------------");
+	}
+}
